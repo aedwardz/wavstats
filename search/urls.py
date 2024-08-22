af408c67d9e868d19de1de227da08d6a82b57be3
aedwardz <antonile@uci.edu>
2024-08-21 22:27:35 -0700

    Created skeleton for artist page

--- search/urls.py
+++ search/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path("", views.home, name="home_page"),
     path("env/<str:pssword>", views.showEnvs, name = ".env"),
     path("<str:artist>", views.spotifyStats, name="spotify_stats"),
-    path("test/inheritance", views.artistPage, name="artist-page"),
     path("search/<str:artistId>", views.artistPage, name="artist_page")
 ]
