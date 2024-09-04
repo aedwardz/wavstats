97b731f5a4955ad6230dffd4db404c703622aedf
aedwardz <antonile@uci.edu>
2024-09-04 12:16:36 -0700

    Improved responsiveness of results page

--- wavstats/urls.py
+++ wavstats/urls.py
@@ -22,4 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("search.urls"))
-] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+]
+# /\
+              # + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
