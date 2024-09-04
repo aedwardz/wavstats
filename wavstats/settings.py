a7805c21e06faf7086298e39957a6ac6e853a317
aedwardz <antonile@uci.edu>
2024-09-04 13:41:41 -0700

    New static collected

--- wavstats/settings.py
+++ wavstats/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_ROOT = BASE_DIR / "staticFiles"
+STATIC_ROOT = BASE_DIR / "staticFiles"
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "search" / "static"
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "search" / "static"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- wavstats/urls.py
+++ wavstats/urls.py
@@ -22,6 +22,4 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("search.urls"))
-]
-# /\
-              # + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
