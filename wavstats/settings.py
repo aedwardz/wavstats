2a88d8d2ed35085f1d787c39411c6e5a2a2561cf
aedwardz <antonile@uci.edu>
2024-09-04 01:00:22 -0700

    production configurations

--- wavstats/settings.py
+++ wavstats/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k&w4x1j%1qllp^^h!l0f2_^t%sk#pgk1y&m9jwrcw=&x!q!4*1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'wavstats.onrender.com',
